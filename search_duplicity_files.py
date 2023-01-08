1e3235af28702ada01240cf104498776b56e8ef9
Jiří Trnečka <trnecka.jiri@gmail.com>
2023-01-09 00:16:34 +0100

    Added program funstion

--- search_duplicity_files.py
+++ search_duplicity_files.py
@@ -1,3 +1,56 @@
+from sqlalchemy.orm import Session
+
+
+def load_files(root_folder: str) -> list:
+    """
+    Loading list all files in root folder and subfolders.
+
+    :param root_folder: str
+    :return: list
+    """
+    pass
+
+
+def db_init(connection_string: str) -> Session:
+    """
+    Initialize database structure.
+
+    :param connection_string: str
+    :return: Session
+    """
+    pass
+
+
+def save_data(init_db_connection: Session) -> bool:
+    """
+    Saving files to the database. Return value is True if saving data was successful.
+
+    :param init_db_connection: Session
+    :return: bool
+    """
+    pass
+
+
+def load_same_files(orig_file: str) -> list:
+    """
+    Loading the same files from database.
+
+    :param orig_file: Path to original file.
+    :return: list
+    """
+    pass
+
+
+def get_hash(path_file: str) -> str:
+    """
+    Getting hash from file
+
+    :param path_file: Path to file.
+    :return: str
+    """
+    pass
+
+
 def search_duplicity_files():
     pass
 
